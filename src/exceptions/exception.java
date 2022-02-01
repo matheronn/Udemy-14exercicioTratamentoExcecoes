c6f5fb9c3932b7e32b99ed97ca8812982fae7719
matheronn <matheusem94@gmail.com>
2022-01-31 21:20:53 -0300

    full program

---
+++ src/exceptions/exception.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class exception extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public exception(String msg) {
+		super(msg);
+	}
+
+}
